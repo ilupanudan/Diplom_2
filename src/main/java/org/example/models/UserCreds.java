db7288cf2ca3d68a9e36e368ce9829e1eef14975
Ilia Mihalev <ilupanudan@yandex.ru>
2024-12-01 16:52:10 +0300

    Using Lombok lib in UserCreds

--- src/main/java/org/example/models/UserCreds.java
+++ src/main/java/org/example/models/UserCreds.java
@@ -1,41 +1,20 @@
 package org.example.models;
+import lombok.*;
 
+import static org.example.steps.UserSteps.user;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCreds {
-    private String email;
-    private String password;
-
-
-    public UserCreds(){}
-
-    public UserCreds(String email, String password){
-        this.email = email;
-        this.password = password;
-
-    }
-
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+   private String email;
+   private String password;
 
     public static UserCreds from(User user) {
         return new UserCreds(user.getEmail(), user.getPassword());
     }
-
-
+    
 }
 
 
